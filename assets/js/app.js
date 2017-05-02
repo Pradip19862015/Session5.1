b09e5ca80efd60df2ea2a8ba7f3c63c89fa573a2
Pradip Kumar Jaiswal <pradip.k.jaiswal@gmail.com>
2017-05-02 11:32:09 +0530

    JAVASCRIPT(I) Assignments session-5

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// Below code about displaying properties in console.
+var name = "John";
+var age = 30;
+var dob = "01-jan-1988";
+var pob = "India";
+
+console.log("%c My Name is " + name + " I am " + age + " years old, & born on " + dob + " in " 
+            + pob + ".", 'background: green; color: white;');
